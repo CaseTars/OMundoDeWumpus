9bd3ac2b08d65b1a685422322d0219b6e1ab6cb6
Thiago Maximo Pavão <thipavao44@gmail.com>
2022-05-09 09:43:24 -0300

    Classes iniciadas: Componente, Heroi, Wumpus, Buraco, Fedor, Brisa

---
+++ src/pt/c40task/l05wumpus/componentes/Brisa.java
@@ -0,0 +1,17 @@
+package pt.c40task.l05wumpus.componentes;
+
+import pt.c40task.l05wumpus.Componente;
+
+public class Brisa extends Componente {
+
+	public Brisa(int x, int y) {
+		super(x, y, 'b', 0);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void interagir(Heroi heroi) {
+		// Sem interacao
+	}
+
+}
---
+++ src/pt/c40task/l05wumpus/componentes/Buraco.java
@@ -0,0 +1,25 @@
+package pt.c40task.l05wumpus.componentes;
+
+import pt.c40task.l05wumpus.Componente;
+
+public class Buraco extends Componente {
+	
+	public Buraco(int x, int y) {
+		super(x, y, 'B', 3);
+	}
+	
+	public void criaBrisa() {
+		for(int dx = -1; dx <= 1; dx++)
+		for(int dy = -1; dy <= 1; dy++) {
+			if(dx == 0 && dy == 0) continue; // sem brisa na posicao do Buraco
+			Brisa brisa = new Brisa(getX() + dx, getY() + dy);
+			getCaverna().adicionarElemento(brisa);
+		}
+	}
+
+	@Override
+	public void interagir(Heroi heroi) {
+		heroi.matar();
+	}
+
+}
